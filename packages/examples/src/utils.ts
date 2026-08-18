e8747a39dff8d2cabb4b4d08d98945a0ebc0de5e
duty1g <dzc0d3r@hotmail.com>
2026-08-18 21:07:37 +0300

    Use darker teal #01726d for ASCII art

--- packages/examples/src/utils.ts
+++ packages/examples/src/utils.ts
@@ -8,7 +8,7 @@ const targetRegex = /(?:(?:([^/@:]*)\/)?(([^@:]+))(?::([^@]*))?@)?(.*)/;
 const credentialRegex = /(?:(?:([^/:]*)\/)?([^:]*)(?::(.*))?)?/;
 
 export const VERSION = '0.1.0';
-const C = '\x1b[38;2;0;211;209m';
+const C = '\x1b[38;2;1;114;109m';
 const X = '\x1b[0m';
 const ASCII_ART = String.raw`
      ____.      __________                __           __
